3114a6a8d44248a5183732ab8baedef4354e7231
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-08-14 22:35:15 +0200

    added layout

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex lg:flex-col justify-center items-center gap-8 min-h-full">
+      <div>Settings</div>
+      <div>Image</div>
+    </main>
+  );
+}
