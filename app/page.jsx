dfbcdcf1c2fab8f25e0b410807fdbca36260c8f9
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-08-16 11:24:45 +0200

    scale & dl image works

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,16 @@
 "use client";
 import { useState } from "react";
+import { ImageGenerator } from "./ImageGenerator";
+import { renderPNG } from "./render-png";
 
 export default function Home() {
-  const [setting, setSettings] = useState({
-    padding: 30,
-    shadow: 10,
-    radius: 80,
+  const [settings, setSettings] = useState({
+    padding: 16,
+    shadow: 30,
+    radius: 16,
   });
   const [image, setImage] = useState();
+  const [disabled, setDisabled] = useState(false);
 
   const handleImage = (e) => {
     const file = e.target.files[0];
@@ -36,62 +39,101 @@ export default function Home() {
   console.log(image);
 
   const handleChange = (e) => {
-    let updatedValue = {};
-    updatedValue = {
-      padding: e.target.value,
-      shadow: e.target.value,
-      radius: e.target.value,
-    };
-    setSettings(updatedValue);
+    const { name, value } = e.target;
+    setSettings((curr) => ({ ...curr, [name]: Number(value) }));
   };
+  console.log(settings);
 
   return (
-    <main className="flex lg:flex-col justify-center items-center gap-8 min-h-full">
-      <div className="flex-col justify-center items-center">
-        <div className="card bg-base-200 w-96 shadow-xl">
-          <div className="card-body">
-            <h2 className="card-title">Settings</h2>
-            <div className="card-actions justify-end">
-              <input
-                type="file"
-                className="file-input file-input-primary w-full max-w-xs"
-                onChange={handleImage}
-              />
-              <p>Padding</p>
-              <input
-                type="range"
-                min={0}
-                max="99"
-                defaultValue={setting.padding}
-                // onChange={(e) => setPadding(e.target.value)}
-                name="padding"
-                className="range range-primary"
-              />
-              <p>Shadow</p>
-
-              <input
-                type="range"
-                min={0}
-                max="99"
-                defaultValue={setting.shadow}
-                name="shadow"
-                className="range range-primary"
-              />
-              <p>Radius</p>
-
-              <input
-                type="range"
-                min={0}
-                max="99"
-                defaultValue={setting.radius}
-                name="radius"
-                className="range range-primary"
-              />
+    <main className="flex max-lg:flex-col justify-center m-auto max-w-4xl items-center gap-8 min-h-full">
+      <div className="card flex-1 bg-base-200 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="font-semibold text-lg">Settings</h2>
+          <label class="form-control w-full max-w-xs">
+            <div class="label">
+              <span class="label-text">Pick a file</span>
             </div>
-          </div>
+            <input
+              type="file"
+              className="file-input file-input-primary w-full max-w-xs"
+              onChange={handleImage}
+            />
+          </label>
+          <label class="form-control w-full max-w-xs">
+            <div class="label">
+              <span class="label-text">Padding</span>
+            </div>
+            <input
+              type="range"
+              min={0}
+              max="99"
+              defaultValue={Number(settings.padding)}
+              onChange={(e) => handleChange(e)}
+              name="padding"
+              className="range range-primary"
+            />
+          </label>
+          <label class="form-control w-full max-w-xs">
+            <div class="label">
+              <span class="label-text">Shadow</span>
+            </div>
+            <input
+              type="range"
+              min={0}
+              max="99"
+              defaultValue={Number(settings.shadow)}
+              onChange={(e) => handleChange(e)}
+              name="shadow"
+              className="range range-primary"
+            />
+          </label>
+          <label class="form-control w-full max-w-xs">
+            <div class="label">
+              <span class="label-text">Radius</span>
+            </div>
+            <input
+              type="range"
+              min={0}
+              max="99"
+              defaultValue={Number(settings.radius)}
+              onChange={(e) => handleChange(e)}
+              name="radius"
+              className="range range-primary"
+            />
+          </label>
         </div>
       </div>
-      <div>{image ? <img src={image.src} /> : null}</div>
+
+      <div
+        // retirer flex - 1
+        style={{ maxWidth: 400 }}
+        className={`w-full h-fit ${
+          image ? "border" : ""
+        } rounded-md  max-w-md lg:max-w-none overflow-hidden h-fit w-fit flex`}
+      >
+        <ImageGenerator settings={settings} image={image} />
+      </div>
+      <button
+        className="btn btn-active btn-primary mt-2"
+        disabled={!image}
+        onClick={async () => {
+          const { blob } = await renderPNG({
+            image,
+            settings,
+          });
+          const url = URL.createObjectURL(blob);
+
+          // Fais en sorte de télécharger l'image ici
+          const element = document.createElement("a");
+          element.download = image.name;
+          element.href = url;
+          document.body.appendChild(element);
+          element.click();
+          setDisabled(true);
+        }}
+      >
+        Download
+      </button>
     </main>
   );
 }
